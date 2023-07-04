6e79a12042b2e6ed0f8f408f4ebef26f79b7a02a
Ayush <kumarayush9694@gmail.com>
2023-07-04 22:33:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var cursor=document.querySelector("#cursor");
+var cursorBlur=document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove",function(dets){
+    cursor.style.left=dets.x+"px";
+    cursor.style.top=dets.y+"px";
+    cursorBlur.style.left=dets.x-250+"px";
+    cursorBlur.style.top=dets.y-250+"px";
+});
