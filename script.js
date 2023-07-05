2ee68d7cfaa707879d252d902e7b490d77f5a58c
Ayush <kumarayush9694@gmail.com>
2023-07-05 23:03:33 +0530

    page2

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ document.addEventListener("mousemove",function(dets){
     cursorBlur.style.left=dets.x-250+"px";
     cursorBlur.style.top=dets.y-250+"px";
 });
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "65px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -5%",
+      end: "top -6%",
+      scrub: 1,
+    },
+  });
+
+  gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+      trigger: "#main",
+      scroller: "body",
+      // markers:true,
+      start: "top -25%",
+      end: "top -70%",
+      scrub: 1,
+    },
+  });
